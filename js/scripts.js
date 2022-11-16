d19870fe110eaade5066eefd77447360197d36be
Valentin Meier <valentinmeier123@gmail.com>
2022-11-16 20:49:55 -0300

    Nosotros terminado y botón de wpp agregado

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,37 @@ fetch(servicios)
   .finally((dat) => {
     console.log(`servicios cargados`);
   });
+
+//* Función para renderizar Nosotros.
+let seccionInformacion = document.querySelector(`.informacionNosotros`);
+if (window.screen.width > 920) {
+  seccionInformacion.innerHTML = `<div class="informacionNosotrosPc">
+  <div class="valoresPc">
+    <h4>Proyecto Cba Composta es una <b>oportunidad de Co-crear</b> espacios de <b>responsabilidad ambiental</b>. Nace en 2021, en el auge del Ecosistema Emprendedor Cordobés; en un intento de dos jóvenes profesionales y amigas; de proponer y fomentar la <b>innovación sustentable en las ciudades.</b></h4>
+    <h4> Iniciativa <b>ganadora del desafio Jóvenes Innovadores</b> del laboratorio de innovación Govetch de la Municipalidad de Córdoba. MVP validado en el sector de la economía circular y Triple impacto. <br> Surgue de la experiencia de su equipo en el trabajo con distintos sectores, sumado al <b>anhelo</b> por sostener las motivaciones personales en un emprendimiento propio.</h4>
+    <h4>Córdoba Composta es una marca de <b>propósitos comunitarios, colaborativos y de equidad,</b> encontrando su identida en la <b>generación de alianzas</b> estratégicas ambientales justas y en red</h4>
+  </div>
+  <div class="imagenNosotrosPc">
+    <img src="./img/composteraCompleta.jpg" alt="Compostera">
+  </div>
+</div>`;
+} else {
+  seccionInformacion.innerHTML = `<div class="informacionNosotrosCelular">
+            
+   <div class="valoresNosotrosCelular">
+     <div class="valorCelular">
+       <img src="./img/CC HOJITAS.png" alt="Hoja valores">
+       <h3>Somos proyecto Córdoba Composta, una oportunidad de Co-crear espacios de responsabilidad ambiental.</h3>
+     </div>
+     <div class="valorCelular">
+       <img src="./img/CC HOJITAS.png" alt="Hoja valores">
+       <h3>Nuestro objetivo es sensibilizar sobre cómo nuestros hábitos,consumos y residuos impactan tanto en nuestros hogares como en nuestra ciudad.</h3>
+     </div>
+     <div class="valorCelular">
+       <img src="./img/CC HOJITAS.png" alt="Hoja valores">
+       <h3>¿Qué hacemos? <br> Generamos alianzas estratégicas,articulando distintos sectores en experiencias de compostaje a gran escala.</h3>
+     </div>
+   </div>
+   <div class="capaNosotros"></div>
+ </div>`;
+}
